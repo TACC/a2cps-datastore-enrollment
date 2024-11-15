1ce3aa0cf51b89054d3c541d5b52bc09373a745d
Chandra Y <cyemparala@tacc.utexas.edu>
2024-11-15 15:41:00 -0600

    more logging adjustments

--- src/app.py
+++ src/app.py
@@ -8,6 +8,8 @@ import logging
 import requests
 import json
 import pandas as pd
+import from gunicorn.glogging import Logger
+
 
 # Dash Framework
 import dash_bootstrap_components as dbc
@@ -321,6 +323,12 @@ app = Dash(__name__,
 
 app.layout = serve_layout
 
+
+class CustomGunicornLogger(Logger):
+    def access(self, resp, req, environ, request_time):
+        super().access(resp, req, environ, request_time)
+        app.logger.info(f"{req.method} {req.path} - {resp.status} in {request_time:.2f}s")
+
 # ----------------------------------------------------------------------------
 # RUN APPLICATION
 # ----------------------------------------------------------------------------
@@ -332,6 +340,7 @@ else:
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+    server.logger_class = CustomGunicornLogger
 
 # ----------------------------------------------------------------------------
 # DATA CALLBACKS
